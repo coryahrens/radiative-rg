dd9bbf3eb102a19121a54f214c4f2a97d27d5174
Nick <nfisher@mines.edu>
2015-06-25 23:58:17 +0700

    added IIPDG code for nonlinear diffusion problem

---
+++ src/python/IIPDG-diffusive-wave.py
@@ -0,0 +1,121 @@
+"""
+Non-Linear diffusive wave problem:
+
+u_t - div(u^8 grad(u)) = 0,
+u(0,t) = 1
+u(1,t) = 0
+u(x,0) = max(1-2x,0)
+
+Method: IIPDG in space and
+backward Euler in time.
+
+"""
+from dolfin import *
+import numpy as np
+import sys
+import matplotlib.pylab as plt
+
+#set_log_level(WARNING)
+
+# Create mesh and define function space
+mesh  = UnitIntervalMesh(400)
+nu    = FacetNormal(mesh)
+h     = CellSize(mesh)
+h_avg = (h('+') + h('-'))/2.0
+V     = FunctionSpace(mesh, 'DG', 1)
+T  = 1.0
+dt = 0.01
+
+
+# Define boundary conditions
+class LeftBoundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return x<DOLFIN_EPS
+class RightBoundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return x>1.-DOLFIN_EPS
+bc_left = DirichletBC(V, Constant(1.0), LeftBoundary(),'pointwise')
+bc_right = DirichletBC(V, Constant(0.0), RightBoundary(), 'pointwise')
+bcs = [bc_left, bc_right]
+
+# Define initial condition
+class fun(Expression):
+    def eval(self, values, x):
+        values[0] = max(1.-2.*x[0],0)
+
+
+# initial condition
+u_old = interpolate(fun(),V)
+
+
+# Define variational problem
+v     = TestFunction(V)
+u     = TrialFunction(V)
+
+alpha = 100.0
+
+def q(u):
+  return u**8.
+
+a = dot(q(u)*grad(u), grad(v))*dx \
+  - avg(dot(q(u)*grad(u),nu))*jump(v)*dS\
+  + alpha/h_avg*jump(u)*jump(v)*dS
+
+F  = (u-u_old)*v*dx + dt*a
+
+
+u_new = Function(V) 
+F     = action(F, u_new)
+J     = derivative(F, u_new, u)
+
+# Compute solution
+problem = NonlinearVariationalProblem(F, u_new, bcs, J)
+solver  = NonlinearVariationalSolver(problem)
+
+
+# Nonlinear solver parameters
+
+prm = solver.parameters
+prm['newton_solver']['absolute_tolerance'] = 1E-10
+prm['newton_solver']['relative_tolerance'] = 1E-9
+prm['newton_solver']['maximum_iterations'] = 30
+prm['newton_solver']['relaxation_parameter'] = 1.0
+prm['newton_solver']['linear_solver'] = 'gmres'
+prm['newton_solver']['preconditioner'] = 'ilu'
+prm['newton_solver']['krylov_solver']['absolute_tolerance'] = 1E-9
+prm['newton_solver']['krylov_solver']['relative_tolerance'] = 1E-7
+prm['newton_solver']['krylov_solver']['maximum_iterations'] = 1000
+prm['newton_solver']['krylov_solver']['gmres']['restart'] = 40
+prm['newton_solver']['krylov_solver']['preconditioner']['ilu']['fill_level'] = 0
+
+# plotting
+plt.ion()
+fig = plt.figure(1)	
+plt.ioff()
+
+# plot func
+def plots(u,xvals,t):
+    plt.cla()
+    plt.plot(xvals,u)
+    plt.ylim(-0.1, 1.1)
+    plt.xlim( 0.0, 1.0)
+    plt.xlabel(r'$x$',fontsize = 15)
+    plt.ylabel(r'$u$',fontsize = 15)
+    plt.title(r'Time: $t$ = ' + str(t).ljust(4, str(0)))
+    plt.grid("on")
+    fig.canvas.draw()
+    return
+
+# extract mesh coordinates
+xvals = mesh.coordinates()
+
+t = dt
+while t <= T:
+    solver.solve()
+    t += dt
+    u_old.assign(u_new)
+    plots(u_new.compute_vertex_values(),xvals,t)
+
+
+plt.show()
+
